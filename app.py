54f43533c950742786efff309d1b34ec2c3545de
yash1th-yerra <yashwanthyash7ms@gmail.com>
2025-03-14 13:01:34 +0530

    tutor

--- app.py
+++ app.py
@@ -3,7 +3,6 @@ import os
 import tempfile
 import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt
 from dotenv import load_dotenv
 from langchain_google_genai import ChatGoogleGenerativeAI
 from langchain.prompts import ChatPromptTemplate, MessagesPlaceholder
